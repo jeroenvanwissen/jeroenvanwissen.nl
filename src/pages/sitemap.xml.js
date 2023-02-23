57e6955c91db3a061376e654bd889e3caefe05cd
Jeroen van Wissen <jeroen.van.wissen@31f.nl>
2023-02-23 22:45:22 +0100

    Trying to fix lastmod date

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -15,16 +15,16 @@ export async function get({ params, request }) {
     const urlset = [];
     const collections = [];
     const categories = [];
-    urlset.push({lastmod: posts[0].data.date, priority: '1.00', loc: import.meta.env.SITE});
+    urlset.push({lastmod: posts[0].data.date.toISOString(), priority: '1.00', loc: import.meta.env.SITE});
     posts.forEach(post => {
         if (!collections.includes(post.collection)) {
-            urlset.push({lastmod: posts[0].data.date, priority: '1.00', loc: `${import.meta.env.SITE}/${post.collection}`});
+            urlset.push({lastmod: posts[0].data.date.toISOString(), priority: '1.00', loc: `${import.meta.env.SITE}/${post.collection}`});
             collections.push(post.collection);
         }
-        urlset.push({lastmod: post.data.date, priority: '0.80', loc: `${import.meta.env.SITE}/blog/${post.slug}`});
+        urlset.push({lastmod: post.data.date.toISOString(), priority: '0.80', loc: `${import.meta.env.SITE}/blog/${post.slug}`});
         generateCategories(post.data.categories).forEach(category => {
             if (!categories.includes(category.slug)) {
-                urlset.push({lastmod: post.data.date, priority: '0.64', loc: `${import.meta.env.SITE}/category/${category.slug}`});
+                urlset.push({lastmod: post.data.date.toISOString(), priority: '0.64', loc: `${import.meta.env.SITE}/category/${category.slug}`});
                 categories.push(category.slug);
             }
         });
