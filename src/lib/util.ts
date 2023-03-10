cc602a4ed760cb9f3ad94bf16fc8f0da730cdc7b
Jeroen van Wissen <jeroen.van.wissen@gmail.com>
2023-03-10 19:49:13 +0100

    More cleanup and possible fix for deploy

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -52,37 +52,4 @@ export async function getEntriesByCategorySlug(slug: string) {
     }    
 }
 
-// export async function getSorted
-
-
-// export async function getSortedCollectionPosts({ collection }) {
-//     return await getCollection(collection)
-//         .then(entries => entries
-//             .filter(entry => !entry.data.draft)
-//             .sort((a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf()));
-// }
-
-// export async function generateSitemapXml({ posts, site}) {
-//     return `
-//     <?xml version="1.0" encoding="UTF-8"?>
-//     <urlset xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-//     <url>
-//         <loc>${site.url}</loc>
-//         <lastmod>${posts[0].data.date.toISOString()}</lastmod>
-//         <priority>1.00</priority>
-//     </url>
-//     ${posts.map(post => {
-//         const loc = new URL(`/${post.collection}/${post.slug}`, site.url).href;
-//         return `
-//             <url>
-//                 <loc>${loc}</loc>
-//                 <lastmod>${post.data.date.toISOString()}</lastmod>
-//                 <priority>0.80</priority>
-//             </url>
-//         `.trim();
-//     }).join("")}
-//     </urlset>
-// `.trim();
-// }
-
 export { }
