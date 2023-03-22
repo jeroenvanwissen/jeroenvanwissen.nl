c9145748db5188b224af1a8c10a552719607fae5
Jeroen van Wissen <jeroen.van.wissen@gmail.com>
2023-03-22 18:24:15 +0100

    Getting rid of Netlify things....

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-[![Netlify Status](https://api.netlify.com/api/v1/badges/c3358a94-db52-4ef3-9b33-646e94750cd9/deploy-status)](https://app.netlify.com/sites/wissen-weblog/deploys)
-
 # jeroenvanwissen.nl
+
+Astro.build based personal website, generates static html files.
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jeroenvanwissen.nl',
   output: 'static',
-  adapter: netlify(),
   integrations: [tailwind()]
 });
