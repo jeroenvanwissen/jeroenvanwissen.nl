44436152ab5f74d70608f0f0c0a48517c6255f06
Jeroen van Wissen <jeroen.van.wissen@gmail.com>
2025-01-14 17:02:50 +0100

    Move to GitHub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jeroenvanwissen.nl',
-  output: 'static',
+//  site: 'https://jeroenvanwissen.nl',
+//  output: 'static',
+  site: 'httsp://jeroenvanwissen.github.io',
+  base: 'jeroenvanwissen.nl',
   integrations: [tailwind()]
 });
