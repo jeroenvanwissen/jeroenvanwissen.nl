d432930f9af0b45c66cbf4bfcca0aa840a15bec7
Jeroen van Wissen <jeroen.van.wissen@gmail.com>
2025-01-14 20:45:35 +0100

    Add old redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,24 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jeroenvanwissen.nl',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  redirects: {
+    '/blog/[...slug]': '/post/[...slug]',
+    '/blog/categories-pages-added': '/post/category-pages-added',
+    '/blog/adding-mdx-and-image-integration': '/',
+    '/contact': '/',
+    '/donated': '/',
+    '/images.json': '/',
+    '/index.xml': '/',
+    '/lastfm-blocks-module': '/',
+    '/lead-story/2008/10/08/howto-iphone-application-development-environment.html': '/',
+    '/over-mij': '/',
+    '/partners': '/',
+    '/portfolio': '/',
+    '/webcam': '/',
+    '/weblog/wordpress/custom-sections': '/',
+    '/weblog/php/howto-generate-animated-gif-with-php': '/',
+    '/weblog/wordpress/add-custom-fields-to-a-taxonomy': '/',
+    '/weblog': '/posts',
+  }
 });
